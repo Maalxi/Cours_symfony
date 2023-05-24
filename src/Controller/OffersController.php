fd56e9869e65ed06b787c49114b3977695341290
Maalxi <maalxi38@gmail.com>
2023-05-24 09:19:01 +0200

    Mise à jour le 24-05-2023 - update

--- src/Controller/OffersController.php
+++ src/Controller/OffersController.php
@@ -11,8 +11,49 @@ class OffersController extends AbstractController
     #[Route('/offers', name: 'app_offers')]
     public function index(): Response
     {
-        return $this->render('offers/index.html.twig', [
+        $page_title = 'Offi';
+        $offers = [
+            [
+                'title' => 'Produit 1',
+                'description' => 'Description du produit 1',
+                'price' => rand(10, 100)
+            ],
+            [
+                'title' => 'Produit 2',
+                'description' => 'Description du produit 2',
+                'price' => rand(10, 100)
+            ],
+            [
+                'title' => 'Produit 3',
+                'description' => 'Description du produit 3',
+                'price' => rand(10, 100)
+            ]
+        ];
+
+        return $this->render('offers/offers.html.twig', [
             'controller_name' => 'OffersController',
+            'page_title' => $page_title,
+            'offers' => $offers,
+        ]);
+    }
+
+    #[Route('/offers/offi2', name: 'app_offi2')]
+    public function offi2(): Response
+    {
+        $page_title = 'Offi2';
+        return $this->render('offers/offi2.html.twig', [
+            'controller_name' => 'OffersController',
+            'page_title' => $page_title
+        ]);
+    }
+
+    #[Route('/offers/offi3', name: 'app_offi3')]
+    public function offi3(): Response
+    {
+        $page_title = 'Offi3';
+        return $this->render('offers/offi3.html.twig', [
+            'controller_name' => 'OffersController',
+            'page_title' => $page_title
         ]);
     }
 }
--- src/Controller/PremierePageController.php
+++ src/Controller/PremierePageController.php
@@ -11,16 +11,20 @@ class PremierePageController extends AbstractController
     #[Route('/', name: 'app_premiere_page')]
     public function index(): Response
     {
+        $page_title = 'Page accueil';
         return $this->render('premiere_page/index.html.twig', [
             'controller_name' => 'PremierePageController',
+            'page_title' => $page_title
         ]);
     }
 
     #[Route('/prpr', name: 'app_prpr')]
     public function test(): Response
     {
+        $page_title = 'PRPR';
         return $this->render('premiere_page/index.html.twig', [
             'controller_name' => 'PremierePageController',
+            'page_title' => $page_title
         ]);
     }
 }
